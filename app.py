f535403fdf97b9083477c8ba234f17387eb70849
John oe <caruso2324@giorgimi.edu.it>
2026-05-30 16:54:31 +0200

    Rimosso overwrite della secret_key

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ app.secret_key = 'chiave_segreta'
 # Configurazione del database e della chiave segreta per i messaggi flash
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
 
